9b40100baa0c876f91f9e8eac5d351c587f45d38
Chad Heisler <chadheisler5@gmail.com>
2021-04-21 10:05:52 -0700

    adjusting height in portfolio

--- src/components/Portfolio/portfolioElements.js
+++ src/components/Portfolio/portfolioElements.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const PortfolioContainer = styled.div`
   display: flex;
+  min-height: 100vh;
   flex-direction: column;
   justify-content: center;
   align-items: center;
