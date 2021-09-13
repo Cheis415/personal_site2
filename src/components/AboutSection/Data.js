807470a9ed1854eb3187c712f39a3bb5bd9d177c
Chad Heisler <chadheisler5@gmail.com>
2021-09-13 15:12:33 -0700

    removed paragraph from about

--- src/components/AboutSection/Data.js
+++ src/components/AboutSection/Data.js
@@ -1,21 +1,23 @@
 export const homeObjOne = {
-  id: 'about',
-  LightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: 'Who am I?',
-  headline: "Started from the bottom",
-  description: `In the last couple years, I started my journey in software development thanks to the guidance and training of Code Tenderloin and the esteemed Rithm School. After years of working my way from Laborer to Lead Supervisor in the construction field, I wanted to finally explore my life-long passion for tech and apply it to a new career. While I am not a typical software engineering applicant, I bring a unique perspective and skill-set to the projects that I work on.
+	id: 'about',
+	LightBg: false,
+	lightText: true,
+	lightTextDesc: true,
+	topLine: 'Who am I?',
+	headline: 'Started from the bottom',
+	description: `In the last couple years, I started my journey in software 
+  development thanks to the guidance and training of Code Tenderloin and the 
+  esteemed Rithm School. After years of working my way from Laborer to Lead 
+  Supervisor in the construction field, I wanted to finally explore my 
+  life-long passion for tech and apply it to a new career. While I am 
+  not a typical software engineering applicant, I bring a unique perspective 
+  and skill-set to the projects that I work on.
   `,
-  description2: `This includes my volunteer work as a mentor to those in need, channeling my experiences with childhood trauma and hardship to serve those going through similar challenges. Between mentorship and my work experience, I bring years of time-sensitive problem solving and collaboration skills (and a healthy sense of humor) to coding. See what I’ve been working on!`,
-  buttonLabel: 'Portfolio',
-  imgStart: false,
-  img: require('../../images/svg-2.svg').default,
-  alt: 'look in the mirror',
-  dark: true,
-  primary: true,
-  darkText: false
-}
-
-
-
+	buttonLabel: 'Portfolio',
+	imgStart: false,
+	img: require('../../images/svg-2.svg').default,
+	alt: 'look in the mirror',
+	dark: true,
+	primary: true,
+	darkText: false,
+};
--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -2,65 +2,61 @@ import React from 'react';
 import { Button } from '../ButtonElement';
 import Languages from '../Languages';
 import {
-  AboutContainer,
-  AboutWrapper,
-  AboutRow,
-  Column1,
-  TextWrapper,
-  TopLine,
-  Heading,
-  TextBody,
-  BtnWrap,
-  Column2,
-  ImgWrap,
-  Img
-} from "./aboutElements";
+	AboutContainer,
+	AboutRow,
+	AboutWrapper,
+	BtnWrap,
+	Column1,
+	Column2,
+	Heading,
+	Img,
+	ImgWrap,
+	TextBody,
+	TextWrapper,
+	TopLine,
+} from './aboutElements';
 
 const AboutSection = ({
-  lightBg, 
-  id, 
-  imgStart, 
-  topLine, 
-  lightText,
-  headline,
-  darkText,
-  description,
-  description2,
-  buttonLabel,
-  img,
-  alt, 
+	lightBg,
+	id,
+	imgStart,
+	topLine,
+	lightText,
+	headline,
+	darkText,
+	description,
+	buttonLabel,
+	img,
+	alt,
 }) => {
-  return (
-    <>
-      <AboutContainer lightB={lightBg} id={id}>
-        <AboutWrapper>
-          <AboutRow imgStart={imgStart}>
-            <Column1>
-              <TextWrapper>
-                <TopLine>{topLine}</TopLine>
-                <Heading lightText={lightText}>{headline}</Heading>
-                <TextBody darkText={darkText}>{description}</TextBody>
-                <TextBody darkText={darkText}>{description2}</TextBody>
-                <BtnWrap>
-                  <Button 
-                  to='projects'
-                  primary="true"
-                  dark="false"
-                  >{buttonLabel}</Button>
-                </BtnWrap>
-              </TextWrapper>
-            </Column1>
-            <Column2>
-              <ImgWrap>
-                <Img src={img} alt={alt}/>
-              </ImgWrap>
-            </Column2>
-          </AboutRow>
-        </AboutWrapper>
-      </AboutContainer>
-      <Languages />
-    </>
-  )
-}
+	return (
+		<>
+			<AboutContainer lightB={lightBg} id={id}>
+				<AboutWrapper>
+					<AboutRow imgStart={imgStart}>
+						<Column1>
+							<TextWrapper>
+								<TopLine>{topLine}</TopLine>
+								<Heading lightText={lightText}>{headline}</Heading>
+								<TextBody darkText={darkText}>{description}</TextBody>
+								<BtnWrap>
+									<Button to="projects" primary="true" dark="false">
+										{buttonLabel}
+									</Button>
+								</BtnWrap>
+							</TextWrapper>
+						</Column1>
+						<Column2>
+							<ImgWrap>
+								<Img src={img} alt={alt} />
+							</ImgWrap>
+						</Column2>
+					</AboutRow>
+				</AboutWrapper>
+			</AboutContainer>
+			<Languages />
+		</>
+	);
+};
 
 export default AboutSection;
